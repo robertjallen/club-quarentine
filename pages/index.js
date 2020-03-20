49fb0f55b42db30082eacd6e95fa92f608ea3660
bobbidigi <bobbidigi34@gmail.com>
2020-03-20 18:43:25 -0400

    add umbrella

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ const Home = () => (
         target="_blank"
         rel="noopener noreferrer"
       >
-        Umbrella Corporation Productions
+        Umbrella Corporation Productions ⛱️
       </a>
     </footer>
 
